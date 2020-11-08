9aefa8e2fbc868522fd3ddcede5a7726b98c1859
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-11-08 17:04:39 +0200

    done

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const { BrowserWindow, app } = require('electron');
+const { BrowserWindow, app, ipcMain, Notification } = require('electron');
+const path = require('path');
+
+const isDev = !app.isPackaged;
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -9,10 +12,21 @@ function createWindow() {
       nodeIntegration: false,
       worldSafeExecuteJavaScript: true,
       contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
   win.loadFile('index.html');
 }
+
+if (isDev) {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+  })
+}
+
+ipcMain.on('notify', (_, message) => {
+  new Notification({ title: 'Notifiation', body: message }).show();
+})
 
 app.whenReady().then(createWindow)
--- src/js/App.js
+++ src/js/App.js
@@ -5,7 +5,12 @@ export default function App() {
 
   return (
     <>
-      <h1>I am App Component!!!</h1>
+      <div id='main'>
+        <h1>click me</h1>
+        <button onClick={() => {
+          electron.notificationApi.sendNotification('My custom notification!');
+        }}>Notify</button>
+      </div>
     </>
   )
 }
